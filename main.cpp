553f9535a23efa400003f77e6af50fa72b04d8b1
JamesCurtis <jamescurtis@foxmail.com>
2022-12-23 15:29:05 +0800

    chore: update

--- main.cpp
+++ main.cpp
@@ -5,8 +5,6 @@
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-//    if (!createTestDatabaseConnection())
-//        return 1;
     MainWindow mainWindow;
     mainWindow.show();
     return QApplication::exec();
--- mainwindow.h
+++ mainwindow.h
@@ -15,12 +15,12 @@
 #include "QItemSelectionModel"
 #include "utils/tools.hpp"
 #include "map"
+#include "QSqlTableModel"
 
 #pragma execution_character_set("utf-8")
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
-class QSqlTableModel;
 
 QT_END_NAMESPACE
 
@@ -32,12 +32,14 @@ public:
 
     ~MainWindow() override;
 
+    /// 课程列表
     QVector<std::tuple<QString, QString>> courseList = {
             {"math",    "高等数学"},
             {"english", "英语"},
             {"compute", "计算机导论"}
     };
 
+    /// 表头信息
     QVector<std::tuple<QString, QString>> tableHeaderList = {
             {"id",      "学号"},
             {"name",    "姓名"},
@@ -48,66 +50,105 @@ public:
     };
 
 private:
+    /// UI窗口指针
     Ui::MainWindow *ui;
+
+    /// 数据模型
     QSqlTableModel *model;
-    const QModelIndex *currentIndex;
+
+    /// 状态栏左侧通知
     QLabel *statusBarLeft = nullptr;
+
+    /// 状态栏右侧通知
     QLabel *statusBarRight = nullptr;
+
+    /// 当前打开的数据库文件名
     QString databaseFileName = "";
+
+    /// 数据库句柄
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+
+    /// QTableView选择模型
     QItemSelectionModel *itemSelectionModel;
 
+    /// 批量绑定所有信号槽
     void bindSlot();
 
+    /// 在底部添加新行
     void handle_menu_action_addNewLine();
 
+    /// 删除当前行
+    void handle_menu_action_removeLine();
+
+    /// 显示统计信息
     void handle_menu_action_showStat();
 
+    /// 打开现有数据库文件
     void handle_menu_action_openDatabase();
 
+    /// 创建新数据库文件
     void handle_menu_action_newDatabase();
 
+    /// 打开测试数据库
     void handle_menu_action_openTestDatabase();
 
+    /// 关闭当前数据库
     void handle_menu_action_closeDatabase();
 
+    /// 导出当前表格和统计信息到Excel
     void handle_menu_action_exportExcel();
 
+    /// 监听模型修改
     void
     handle_model_dataChanged(const QModelIndex &topLeft, const QModelIndex &bottomRight, const QVector<int> &roles);
 
+    /// 初始化状态栏
     void initStatusBar();
 
+    /// 重新计算给定行的平均成绩
     void reCalcAverage(const QModelIndex &index);
 
+    /// 刷新状态栏显示
     void refreshStatusBar();
 
+    /// 初始化表格数据模型
     void initModel(QString &table);
 
     void initModel();
 
+    /// 设置状态栏分数显示
     void setScoreInStatusBar(StudentModel *studentModel);
 
+    /// 刷新UI显示
     void refreshUI();
 
+    /// 重新连接数据库
     bool reconnectDatabase(QString &name);
 
+    /// 重建数据库
     bool reBuildDatabase(QString &name);
 
+    /// 检查数据库可用性
     bool checkDatabase();
 
 private slots:
 
+    /// 监听tableView右键
     [[maybe_unused]] void on_tableView_customContextMenuRequested(const QPoint &pos);
 
+    /// 监听tableView鼠标按下
     [[maybe_unused]] void on_tableView_pressed(const QModelIndex &index);
 
+    /// 保存按钮被点击
     [[maybe_unused]] void on_pushButton_Save_clicked();
 
+    /// 还原修改按钮被点击
     [[maybe_unused]] void on_pushButton_Revert_clicked();
 
+    /// 重新载入数据库按钮被点击
     [[maybe_unused]] void on_pushButton_Reload_clicked();
 
+    /// 搜索按钮被点击
     [[maybe_unused]] void on_pushButton_search_clicked();
 };
 
